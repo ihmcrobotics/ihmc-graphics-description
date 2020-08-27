2f40f4ff4d1a6522b9518c3e0161ef73d02a7b48
Sylvain Bertrand <sbertrand@ihmc.us>
2020-08-27 16:45:13 -0500

    :arrow_up: euclid 0.15.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -19,8 +19,8 @@ mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
    api("org.apache.commons:commons-lang3:3.9")
 
-   api("us.ihmc:euclid-frame:0.15.0")
-   api("us.ihmc:euclid-shape:0.15.0")
+   api("us.ihmc:euclid-frame:0.15.1")
+   api("us.ihmc:euclid-shape:0.15.1")
    api("us.ihmc:ihmc-commons:0.30.2")
    api("us.ihmc:ihmc-yovariables:0.9.3")
 }
