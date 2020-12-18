ca18483c8cd666badac687d967cf2a9902bbdf74
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-18 16:31:11 -0600

    :arrow_up: ihmc-yovariables 0.9.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ mainDependencies {
    api("us.ihmc:euclid-frame:0.16.0")
    api("us.ihmc:euclid-shape:0.16.0")
    api("us.ihmc:ihmc-commons:0.30.4")
-   api("us.ihmc:ihmc-yovariables:0.9.7")
+   api("us.ihmc:ihmc-yovariables:0.9.8")
 }
 
 testDependencies {
