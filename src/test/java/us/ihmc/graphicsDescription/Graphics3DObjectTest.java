951b007860e98e62673a121bb4929ddda906d588
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-22 18:51:14 -0600

    Fixed some compilation error in the test

--- src/test/java/us/ihmc/graphicsDescription/Graphics3DObjectTest.java
+++ src/test/java/us/ihmc/graphicsDescription/Graphics3DObjectTest.java
@@ -15,7 +15,7 @@ public class Graphics3DObjectTest
    public void testValidCubeGraphics()
    {
       Graphics3DObject cubeGraphics = new Graphics3DObject();
-      cubeGraphics.translate(new Vector3D(CUBE_X, 0.0, 0.0));
+      cubeGraphics.appendTranslation(new Vector3D(CUBE_X, 0.0, 0.0));
       cubeGraphics.addCube(CUBE_SIDE, CUBE_SIDE, CUBE_SIDE);
 
       assertEquals(2, cubeGraphics.getGraphics3DInstructions().size());
