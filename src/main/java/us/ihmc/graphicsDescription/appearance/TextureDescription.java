ae5ba068499ff5ac0505e1065b4ce0a2b0cdefd8
Sylvain Bertrand <sbertrand@ihmc.us>
2020-12-29 16:22:45 -0600

    Added option to use a simple String to point to a file.

--- src/main/java/us/ihmc/graphicsDescription/appearance/TextureDescription.java
+++ src/main/java/us/ihmc/graphicsDescription/appearance/TextureDescription.java
@@ -8,6 +8,7 @@ import us.ihmc.graphicsDescription.geometry.ExtrusionDescription;
 public class TextureDescription
 {
    private URL textureFileURL;
+   private String textureFilename;
    private BufferedImage textureImage;
 
    public TextureDescription()
@@ -17,12 +18,18 @@ public class TextureDescription
    public TextureDescription(TextureDescription other)
    {
       textureFileURL = other.textureFileURL;
+      textureFilename = other.textureFilename;
       textureImage = ExtrusionDescription.copyImage(other.textureImage);
    }
 
    public void setTextureFileURL(URL textureFileURL)
    {
       this.textureFileURL = textureFileURL;
+   }
+
+   public void setTextureFilename(String textureFilename)
+   {
+      this.textureFilename = textureFilename;
    }
 
    public void setTextureImage(BufferedImage textureImage)
@@ -33,6 +40,11 @@ public class TextureDescription
    public URL getTextureFileURL()
    {
       return textureFileURL;
+   }
+
+   public String getTextureFilename()
+   {
+      return textureFilename;
    }
 
    public BufferedImage getTextureImage()
