dad3fa4d78c29db098bbed2f08e77d8b5befa9b6
Duncan Calvert <dcalvert@ihmc.us>
2022-10-28 20:51:23 -0500

    :arrow_up: 0.32.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,10 +20,10 @@ mainDependencies {
 
    api("us.ihmc:euclid-frame:0.19.0")
    api("us.ihmc:euclid-shape:0.19.0")
-   api("us.ihmc:ihmc-commons:0.31.0")
+   api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:ihmc-yovariables:0.9.16")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.31.0")
+   api("us.ihmc:ihmc-commons-testing:0.32.0")
 }
