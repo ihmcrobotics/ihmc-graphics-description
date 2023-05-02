fa93fd0ca3db64aa481ecb91bdac893567af1e98
smccrory <smccrory@ihmc.us>
2023-05-02 08:50:10 -0500

    :arrow_up: euclid 0.20.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,8 +18,8 @@ ihmc {
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
 
-   api("us.ihmc:euclid-frame:0.19.1")
-   api("us.ihmc:euclid-shape:0.19.1")
+   api("us.ihmc:euclid-frame:0.20.0")
+   api("us.ihmc:euclid-shape:0.20.0")
    api("us.ihmc:ihmc-commons:0.32.0")
    api("us.ihmc:ihmc-yovariables:0.9.17")
    api("us.ihmc:scs2-definition:17-0.14.1")
