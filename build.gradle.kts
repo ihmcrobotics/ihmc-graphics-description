c1cc24c7435a6804b5abaa64f421dd3701ebb3ef
nkitchel <nkitchel@ihmc.org>
2024-12-17 11:12:28 -0600

    :bookmark: 0.21.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 ihmc {
    group = "us.ihmc"
-   version = "0.20.8"
+   version = "0.21.0"
    vcsUrl = "https://github.com/ihmcrobotics/ihmc-graphics-description"
    openSource = true
 
