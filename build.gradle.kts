42e66ca4f9e652c6dd80db0777ccd023cd99f77f
nkitchel <nkitchel@ihmc.org>
2024-12-17 11:09:27 -0600

    Update dependency version, pull everything from simulation-construction-set-group

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,8 +1,6 @@
 plugins {
    id("us.ihmc.ihmc-build")
-   id("us.ihmc.log-tools-plugin") version "0.6.3"
-   id("us.ihmc.ihmc-ci") version "8.3"
-   id("us.ihmc.ihmc-cd") version "1.26"
+   id("us.ihmc.log-tools-plugin") version "0.6.4"
 }
 
 ihmc {
@@ -18,13 +16,13 @@ ihmc {
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
 
-   api("us.ihmc:euclid-frame:0.20.0")
-   api("us.ihmc:euclid-shape:0.20.0")
-   api("us.ihmc:ihmc-commons:0.32.0")
-   api("us.ihmc:ihmc-yovariables:0.10.0")
-   api("us.ihmc:scs2-definition:17-0.17.0")
+   api("us.ihmc:euclid-frame:0.22.2")
+   api("us.ihmc:euclid-shape:0.22.2")
+   api("us.ihmc:ihmc-commons:0.34.0")
+   api("us.ihmc:ihmc-yovariables:0.13.3")
+   api("us.ihmc:scs2-definition:17-0.28.2")
 }
 
 testDependencies {
-   api("us.ihmc:ihmc-commons-testing:0.32.0")
+   api("us.ihmc:ihmc-commons-testing:0.34.0")
 }
