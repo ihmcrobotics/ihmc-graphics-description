7954bda97139a123053f402eab2c7b7080a835b8
Dexton Anderson <danderson@ihmc.org>
2025-02-05 15:47:15 -0600

    :arrow_up: euclid 0.22.3

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ ihmc {
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
 
-   api("us.ihmc:euclid-frame:0.22.2")
-   api("us.ihmc:euclid-shape:0.22.2")
+   api("us.ihmc:euclid-frame:0.22.3")
+   api("us.ihmc:euclid-shape:0.22.3")
    api("us.ihmc:ihmc-commons:0.34.0")
    api("us.ihmc:ihmc-yovariables:0.13.4")
    api("us.ihmc:scs2-definition:17-0.28.3")
