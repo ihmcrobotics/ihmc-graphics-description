650844f57fcf72feffa01e00a8cde2cc812d0e2b
beomyeong <bpark@ihmc.org>
2026-02-02 17:57:05 -0600

    :arrow_up: euclid to 0.22.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -16,8 +16,8 @@ ihmc {
 mainDependencies {
    api("net.sf.trove4j:trove4j:3.0.3")
 
-   api("us.ihmc:euclid-frame:0.22.3")
-   api("us.ihmc:euclid-shape:0.22.3")
+   api("us.ihmc:euclid-frame:0.22.5")
+   api("us.ihmc:euclid-shape:0.22.5")
    api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-yovariables:0.13.7")
    api("us.ihmc:scs2-definition:17-0.31.3")
