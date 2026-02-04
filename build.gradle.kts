cce30b3c3dd2ae40c144ed041660a4a069aefdce
Robert <rjgriffin42@gmail.com>
2026-02-04 16:47:50 -0600

    :arrow_up: scs2-definition 17-0.32.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -20,7 +20,7 @@ mainDependencies {
    api("us.ihmc:euclid-shape:0.22.5")
    api("us.ihmc:ihmc-commons:0.35.1")
    api("us.ihmc:ihmc-yovariables:0.13.7")
-   api("us.ihmc:scs2-definition:17-0.31.3")
+   api("us.ihmc:scs2-definition:17-0.32.0")
 }
 
 testDependencies {
